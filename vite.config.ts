214f8d17b7067ef5c398f1cc0ba3c0b488730ad1
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-10-09 19:08:25 +0530

    Added PWA support (?)

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: "Gamemaster",
+        short_name: "gamemaster_app",
+        start_url: ".",
+        display: "standalone",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "512x512",
+            type: "image/x-icon",
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
