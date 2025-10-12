566414fbe60490f83b07eb378a0f87e6ce3da2c0
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-10-12 15:08:34 +0530

    Attempt #2 at fixing touch controls for status effect

--- src/components/StatusEffect.vue
+++ src/components/StatusEffect.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="effect-container" @mouseenter="">
+  <div class="effect-container">
     <p class="effect-text" :class="{ buff: props.effect.buff, debuff: !props.effect.buff }"
       @mouseenter="showDelete = true" @mouseleave="showDelete = false" @touchstart="showDelete = !showDelete">
-      {{ effectSign }} {{ props.effect.text }} <span class="effect-delete" v-show="showDelete"
+      {{ effectSign }} {{ props.effect.text }} <span class="effect-delete" v-show="showDelete" @touchstart.stop
         @click="deleteEffect">&times;</span>
     </p>
   </div>
